ccaf9dc038dd450b9b723d215198104acd24fea1
William Banks <williamjb555@gmail.com>
2022-03-14 22:56:05 +0000

    Progess with storing stuff in VulkanCompute

--- src/fractalgen/gpu_renderer/compute/mod.rs
+++ src/fractalgen/gpu_renderer/compute/mod.rs
@@ -1,87 +1,83 @@
 pub mod shaders;
 
-use vulkano::instance::{Instance, InstanceExtensions};
-use vulkano::Version;
+use vulkano::instance::{Instance};
 use vulkano::device::physical::{PhysicalDevice, PhysicalDeviceType, QueueFamily};
-use vulkano::device::{Device, DeviceExtensions, Features};
+use vulkano::device::{Device, DeviceExtensions, Features, Queue};
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage};
 use vulkano::sync::{self, GpuFuture};
 use vulkano::pipeline::{Pipeline, ComputePipeline, PipelineBindPoint};
 use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
 
-use vulkano_win::VkSurfaceBuild;
-use winit::event_loop::{EventLoop, ControlFlow};
-use winit::window::{WindowBuilder, Window};
-use winit::event::{Event, WindowEvent};
-use vulkano::swapchain::Surface;
-
 use super::super::super::fractalgen::{PlaneTransform, FractalType, ImageBufferFormat};
 use std::sync::Arc;
 
 pub struct VulkanCompute {
+	physical: PhysicalDevice<'static>,
+	queue_family: QueueFamily<'static>,
+	device: Arc<Device>,
+	queue: Arc<Queue>
 }
 
 impl VulkanCompute {
-	pub fn render_fractal_to(img_buffer_fmt: ImageBufferFormat, fractal_type: FractalType, dimensions: (u32, u32), transform: &PlaneTransform<f64>, max_iterations: Option<u32>) {
+	pub fn new(vk_instance: &'static Arc<Instance>) -> Self {
+		let device_extensions = DeviceExtensions {
+			..DeviceExtensions::none()
+		};
+
+		let (physical, queue_family) = VulkanCompute::select_compute_device(&vk_instance, &device_extensions);
+
+		println!("Using vulkan device: {} (type: {:?})", physical.properties().device_name, physical.properties().device_type);
+
+		let (device, mut queues) = Device::new(
+			physical,
+			&Features::none(),
+			&physical.required_extensions().union(&device_extensions),
+			[(queue_family, 0.5)].iter().cloned()
+		).expect("Failed to create a device");
+
+		let queue = queues.next().unwrap();
+
+		VulkanCompute {
+			physical,
+			queue_family,
+			device,
+			queue
+		}
+	}
+
+	pub fn select_compute_device(instance: &'static Arc<Instance>, device_extensions: &DeviceExtensions) -> (PhysicalDevice<'static>, QueueFamily<'static>) {
+		PhysicalDevice::enumerate(&instance)
+			.filter(|&p| p.supported_extensions().is_superset_of(&device_extensions))
+			.filter_map(|p| {
+				// The Vulkan specs guarantee that a compliant implementation must provide at least one queue that supports compute operations
+				p.queue_families()
+					.find(|&q| q.supports_compute())
+					.map(|q| (p, q))
+			})
+			.min_by_key(|(p, _)| match p.properties().device_type {
+				PhysicalDeviceType::DiscreteGpu => 0,
+				PhysicalDeviceType::IntegratedGpu => 1,
+				PhysicalDeviceType::VirtualGpu => 2,
+				PhysicalDeviceType::Cpu => 3,
+				PhysicalDeviceType::Other => 4
+			}).expect("No vulkan implementations found")
+	}
+
+	pub fn render_fractal_to(&self, img_buffer_fmt: ImageBufferFormat, fractal_type: FractalType, dimensions: (u32, u32), transform: &PlaneTransform<f64>, max_iterations: Option<u32>) {
 		println!("Starting Vulkan stuff...");
 
 		let (width, height) = dimensions;
 
-		// Create a vulkan instance
-		let vk_instance = Instance::new(None, Version::V1_1, &InstanceExtensions::none(), None).expect("Failed to create vulkan instance");
-
-		// Get the first vulkan device
-		let physical = PhysicalDevice::enumerate(&vk_instance).next().expect("No devices supporting vulkan available");
-
-		// Find a queue family that supports graphics
-		let queue_family = physical.queue_families().find(|&q| q.supports_graphics()).expect("Couldn't find a graphics queue family");
-
-		let (device, mut queues) = Device::new(physical, &Features::none(), &DeviceExtensions::none(), [(queue_family, 0.5)].iter().cloned()).expect("Failed to create device");
-
-		let queue = queues.next().unwrap();
-
-		// let data = [0; 128];
-		// let buffer = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), false, data).expect("Failed to create buffer"); // device.clone clones the Arc
-
-		if false { // Copy buffer example
-			let source_dat = 0..64; // Iterator that produces 64 values of 0 to 63
-			let source_buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, source_dat).expect("Failed to create buffer");
-
-			let dest_dat = (0..64).map(|_| 0); // Iterator for 64 0s
-			let dest_buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, dest_dat).expect("Failed to create buffer");
-
-			// Create a primary command buffer builder
-			let mut com_buf_builder = AutoCommandBufferBuilder::primary(device.clone(), queue.family(), CommandBufferUsage::OneTimeSubmit).expect("Failed to create command buffer builder");
-
-			// Add the copy buffer command to command buffer builder
-			com_buf_builder.copy_buffer(source_buf.clone(), dest_buf.clone()).expect("Failed to add command to command buffer builder");
-
-			// Turn the command buffer builder into an actual command buffer
-			let command_buffer = com_buf_builder.build().expect("Failed to create command buffer");
-
-			let future = sync::now(device.clone())
-				.then_execute(queue.clone(), command_buffer)
-				.unwrap()
-				.then_signal_fence_and_flush() // Same as signal fence followed by flush
-				.expect("Flush error");
-
-			future.wait(None).unwrap(); // None is optional timeout
-
-			let source_content = source_buf.read().expect("Failed to read buffer");
-			let dest_content = dest_buf.read().expect("Failed to read buffer");
-			assert_eq!(&*source_content, &*dest_content);
-		}
-
 		if true { // Render mandelbrot example
 			// Create data buffer to be operated on
 			let data_dat = (0..(width * height)).map(|_| 0);
-			let data_buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, data_dat).expect("Failed to create buffer");
+			let data_buf = CpuAccessibleBuffer::from_iter(self.device.clone(), BufferUsage::all(), false, data_dat).expect("Failed to create buffer");
 
-			let shader = shaders::mandelbrot::compute::load(device.clone()).expect("Failed to create shader module");
+			let shader = shaders::mandelbrot::compute::load(self.device.clone()).expect("Failed to create shader module");
 
 			let compute_pipeline = ComputePipeline::new(
-				device.clone(),
+				self.device.clone(),
 				shader.entry_point("main").unwrap(),
 				&(),
 				None,
@@ -96,7 +92,7 @@ impl VulkanCompute {
 
 			let set = PersistentDescriptorSet::new(layout.clone(), [WriteDescriptorSet::buffer(0, data_buf.clone())]).unwrap();
 
-			let mut builder = AutoCommandBufferBuilder::primary(device.clone(), queue.family(), CommandBufferUsage::OneTimeSubmit).unwrap();
+			let mut builder = AutoCommandBufferBuilder::primary(self.device.clone(), self.queue.family(), CommandBufferUsage::OneTimeSubmit).unwrap();
 
 			builder.bind_pipeline_compute(compute_pipeline.clone())
 				.bind_descriptor_sets(
@@ -110,8 +106,8 @@ impl VulkanCompute {
 
 			let command_buffer = builder.build().unwrap();
 
-			let future = sync::now(device.clone())
-				.then_execute(queue.clone(), command_buffer)
+			let future = sync::now(self.device.clone())
+				.then_execute(self.queue.clone(), command_buffer)
 				.unwrap()
 				.then_signal_fence_and_flush()
 				.unwrap();
